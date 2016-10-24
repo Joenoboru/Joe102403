e87ec027ecae76a19afea61f5b5bae1d414f5223
joenoboru <joe4580803@gmail.com>
2016-10-24 14:39:18 +0800

    spinner 與 ArrayAdapter應用

---
+++ app/src/main/java/com/example/user/joe102403/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.user.joe102403;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    TextView tv,tv2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        tv = (TextView)findViewById(R.id.textView);
+        tv2 = (TextView)findViewById(R.id.textView2);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TextView itemTv = (TextView) view;
+                //String str = itemTv.getText().toString();
+                String[] f=getResources().getStringArray(R.array.fruits);
+                String str=f[position];
+                tv.setText(str);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+    public void click1(View v)
+    {
+        int p =spinner.getSelectedItemPosition();
+        String[] f=getResources().getStringArray(R.array.fruits);
+        String str=f[p];
+        tv2.setText(str);
+    }
+}
